cae6a556d8fc3906f91f9df3d40ca9059df15102
Quentin Champenois <26109239+Quentinchampenois@users.noreply.github.com>
2023-06-16 15:17:34 +0200

    Refactor Rack Attack configuration (#312)
    * Add proxy and remote_ip to rack attack
    
    * Add Rack Attack configuration
    
    * Add unauthorized path for fail2ban
    
    * Refactor Rack Attack configuration
    
    * Load decidim-app config
    
    * Fix rubocop offenses
    
    * Allow to add proxy ip to Rack Attack and Action Dispatch safelist
    
    * Add application.rb specs
    
    * Update secrets.yml
    
    * Add specs for DecidimApp config
    
    * Add DecidimApp Rack Attack specs
    
    * Remove spec file
    
    * Add rack attack throttling specs
    
    * Add specs for Fail2ban
    
    * Revert changes in db/schema.rb
    
    * Fix not interpreted error
    
    * Remove trusted proxies configuration
    
    * Remove trusted proxies configuration
    # Conflicts:
    #	config/initializers/decidim.rb
    #	config/initializers/rack_attack.rb
    #	config/locales/fr.yml

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,40 +1,59 @@
 # frozen_string_literal: true
 
-if Rails.env.production?
-  # Remove the original throttle fron decidim-core
-  # see https://github.com/decidim/decidim/blob/release/0.26-stable/decidim-core/config/initializers/rack_attack.rb#L19
-  Rails.application.config.after_initialize do
-    Rack::Attack.throttles.delete("requests by ip")
-  end
-  Rack::Attack.throttle("req/ip",
-                        limit: Decidim.throttling_max_requests,
-                        period: Decidim.throttling_period) do |req|
-    next if req.path.start_with?("/decidim-packs")
-    next if req.path.start_with?("/rails/active_storage")
+require "decidim-app/rack_attack"
+require "decidim-app/rack_attack/throttling"
+require "decidim-app/rack_attack/fail2ban"
 
-    req.ip
-  end
-  ActiveSupport::Notifications.subscribe("throttle.rack_attack") do |name, start, finish, request_id, payload|
-    # request object available in payload[:request]
+# Enabled by default in production
+# Can be deactivated with 'ENABLE_RACK_ATTACK=0'
+Rack::Attack.enabled = DecidimApp::RackAttack.rack_enabled?
+return unless Rack::Attack.enabled
 
-    rack_logger = Logger.new(Rails.root.join("log/rack_attack.log"))
+# Remove the original throttle from decidim-core
+# see https://github.com/decidim/decidim/blob/release/0.26-stable/decidim-core/config/initializers/rack_attack.rb#L19
+DecidimApp::RackAttack::Throttling.deactivate_decidim_throttling! do
+  Rails.logger.info("Deactivating 'requests by ip' from Decidim Core")
+  Rack::Attack.throttles.delete("requests by ip")
+end
 
-    request = payload[:request]
+Rack::Attack.throttled_response_retry_after_header = true
+# By default use the memory store for inspecting requests
+# Better to use MemCached or Redis in production mode
+Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new if !ENV["MEMCACHEDCLOUD_SERVERS"] || Rails.env.test?
 
-    params = {
-      "name" => name,
-      "start" => start,
-      "finish" => finish,
-      "request_id" => request_id,
-      "payload" => request.instance_variable_get(:@env)["rack.attack.match_data"],
-      "ip" => request.ip,
-      "path" => request.path,
-      "get" => request.GET,
-      "post" => request.POST,
-      "host" => request.host,
-      "referer" => request.referer
-    }
+Rack::Attack.throttled_responder = lambda do |request|
+  rack_logger = Logger.new(Rails.root.join("log/rack_attack.log"))
+  throttling_limit = DecidimApp::RackAttack::Throttling.time_limit_for(request.env["rack.attack.match_data"])
 
-    rack_logger.warn("[Rack::Attack] [THROTTLE - req / ip] | #{params}")
+  request_uuid = request.env["action_dispatch.request_id"]
+  params = {
+    "ip" => request.ip,
+    "path" => request.path,
+    "get" => request.GET,
+    "host" => request.host,
+    "referer" => request.referer
+  }
+
+  rack_logger.warn("[#{request_uuid}] #{params}")
+
+  [429, { "Content-Type" => "text/html" }, [DecidimApp::RackAttack::Throttling.html_template(throttling_limit, request.env["decidim.current_organization"]&.name)]]
+end
+
+Rack::Attack.throttle(DecidimApp::RackAttack::Throttling.name,
+                      limit: DecidimApp::RackAttack::Throttling.max_requests,
+                      period: DecidimApp::RackAttack::Throttling.period) do |req|
+  req.ip unless DecidimApp::RackAttack::Throttling.authorized_path?(req.path)
+end
+
+if DecidimApp::RackAttack::Fail2ban.enabled?
+  # Block suspicious requests made for pentesting
+  # After 1 forbidden request, block all requests from that IP for 1 hour.
+  Rack::Attack.blocklist("fail2ban pentesters") do |req|
+    # `filter` returns truthy value if request fails, or if it's from a previously banned IP
+    # so the request is blocked
+    Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 0, findtime: 10.minutes, bantime: 1.hour) do
+      # The count for the IP is incremented if the return value is truthy
+      DecidimApp::RackAttack::Fail2ban.unauthorized_path?(req.path)
+    end
   end
 end
