79be4958e44ee3cdc22bccf33a84988b6482b2c0
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2024-05-02 13:59:40 +0200

    feat: Addition of Extra User Fields Module (#79)
    * feat: Add Extra User Fields
    
    * lint: Rubocop Fixes

---
+++ app/commands/concerns/decidim/extra_user_fields/create_registrations_commands_overrides.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require "active_support/concern"
+
+module Decidim
+  module ExtraUserFields
+    # Changes in methods to store extra fields in user profile
+    module CreateRegistrationsCommandsOverrides
+      extend ActiveSupport::Concern
+
+      def call
+        return broadcast(:invalid) if same_email_representative?
+
+        if form.invalid?
+
+          user = User.has_pending_invitations?(form.current_organization.id, form.email)
+          user.invite!(user.invited_by) if user
+          return broadcast(:invalid)
+        end
+
+        create_user
+        send_email_to_statutory_representative
+
+        broadcast(:ok, @user)
+      rescue ActiveRecord::RecordInvalid
+        broadcast(:invalid)
+      end
+
+      private
+
+      def create_user
+        @user = User.create!(
+          email: form.email,
+          name: form.name,
+          nickname: form.nickname,
+          password: form.password,
+          password_confirmation: form.password_confirmation,
+          password_updated_at: Time.current,
+          organization: form.current_organization,
+          tos_agreement: form.tos_agreement,
+          accepted_tos_version: form.current_organization.tos_version,
+          locale: form.current_locale,
+          extended_data: extended_data
+        )
+      end
+
+      def extended_data
+        @extended_data ||= (@user&.extended_data || {}).merge(
+          country: form.country,
+          postal_code: form.postal_code,
+          date_of_birth: form.date_of_birth,
+          gender: form.gender,
+          phone_number: form.phone_number,
+          location: form.location,
+          underage: form.underage,
+          statutory_representative_email: form.statutory_representative_email
+        )
+      end
+
+      def send_email_to_statutory_representative
+        return if form.statutory_representative_email.blank? || !form.underage
+
+        Decidim::ExtraUserFields::StatutoryRepresentativeMailer.inform(@user).deliver_now
+      end
+
+      def same_email_representative?
+        return false if form.statutory_representative_email.blank?
+
+        form.statutory_representative_email == form.email
+      end
+    end
+  end
+end
