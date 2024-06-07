d43d066145d4d1e89f37c81fce19dbd90f08ad73
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2024-06-07 09:40:39 +0200

    fix: Select first photo in proposal form

--- app/forms/decidim/proposals/proposal_form.rb
+++ app/forms/decidim/proposals/proposal_form.rb
@@ -47,7 +47,7 @@ module Decidim
         # Proposals have the "photos" field reserved for the proposal card image
         # so we don't want to show all photos there. Instead, only show the
         # first photo.
-        self.photos = [model.photos.first].compact.select { |p| p.weight.zero? }
+        self.photos = [model.photo].compact.select { |p| p.weight.zero? }
         self.documents = model.attachments - photos
       end
 
