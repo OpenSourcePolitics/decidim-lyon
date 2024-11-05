92ff3337411b3da53cdfae4bc108d812f8ae758d
Quentin Champenois <quentin.champenois7@gmail.com>
2024-11-05 15:41:10 +0100

    fix: Add rack mini profiler on production mode

--- app/controllers/decidim_controller.rb
+++ app/controllers/decidim_controller.rb
@@ -4,4 +4,17 @@
 # entry point, but you can change what controller it inherits from
 # so you can customize some methods.
 class DecidimController < ApplicationController
+  before_action :enable_rack_mini_profiler
+
+  def enable_rack_mini_profiler
+    Rack::MiniProfiler.authorize_request if profiler?
+  end
+
+  private
+
+  def profiler?
+    return false if Rails.application.secrets.dig(:profiler, :key).blank?
+
+    request.headers["X-Rack-Mini-Profiler-UUID"] == Rails.application.secrets.dig(:profiler, :key)
+  end
 end
