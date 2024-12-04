c5367b215165bcf36592afb5692de8acb3c806ba
Quentin Champenois <26109239+Quentinchampenois@users.noreply.github.com>
2024-12-04 16:12:32 +0100

    feat: Export proposals attachments url (#126)
    * fix: Export proposals rake task
    
    * fix: Export attachments url in proposal serializer
    
    * revert: Remove dummy rake task
    
    * spec: Ensure attachments_urls key is serialized

--- lib/extends/serializers/proposals/proposals_serializer_extends.rb
+++ lib/extends/serializers/proposals/proposals_serializer_extends.rb
@@ -33,6 +33,7 @@ module ProposalSerializerExtends
       },
       comments: proposal.comments_count,
       attachments: proposal.attachments.count,
+      attachment_urls: attachment_urls,
       followers: proposal.followers.count,
       published_at: proposal.published_at,
       url: url,
@@ -44,6 +45,12 @@ module ProposalSerializerExtends
         url: original_proposal_url
       }
     }
+  end
+
+  def attachment_urls
+    proposal.attachments.map do |attc|
+      Rails.application.routes.url_helpers.rails_blob_url(attc.file.blob, host: proposal.participatory_space.organization.host)
+    end.join(",")
   end
 end
 
